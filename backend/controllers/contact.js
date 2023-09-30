c2a438a51ef9fb353c2d9d3d07c1af36a65a5edb
Codecamp-Preview <ahmadramadanirpl@gmail.com>
2023-09-30 19:38:31 +0700

    update: documentation api

--- backend/controllers/contact.js
+++ backend/controllers/contact.js
@@ -3,30 +3,27 @@ const router = express.Router();
 const contact = require("../model/Contact");
 
 router.get("/", (req, res) => {
-  res.json({
-    status: 200,
-    payload: "default router",
-  });
+  res.send('<a href="/api-docs">Documentation</a>');
 });
 
 router.get("/contacts", (req, res) => {
   contact.getAll(req, res);
 });
 
-router.get("/contacts/:id", (req, res) => {
-  contact.show(req, res);
-});
-
 router.post("/contacts", (req, res) => {
   contact.add(req, res);
 });
 
-router.delete("/contacts/:id", (req, res) => {
-  contact.drop(req, res);
+router.get("/contacts/:id", (req, res) => {
+  contact.show(req, res);
 });
 
 router.patch("/contacts/:id", (req, res) => {
   contact.edit(req, res);
+});
+
+router.delete("/contacts/:id", (req, res) => {
+  contact.drop(req, res);
 });
 
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,20 @@ const port = 8000;
 const contact = require("./controllers/contact");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const apiDocumentation = require("./apidoc.json");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 app.use("/api", contact);
+
+app.get("/", (req, res) => {
+  res.send("API READY..");
+});
 
 app.listen(port, () => {
   console.log(`server nyala di localhost:${port}`);
